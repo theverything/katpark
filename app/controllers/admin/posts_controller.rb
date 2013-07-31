3f84d604dc5fc94a3984a1a487d4142efc6b063e
theverything <j3ffhorn@gmail.com>
2013-07-30 22:48:04 -0700

    don't use tables for admin posts

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,7 +2,7 @@ class Admin::PostsController < Admin::BaseController
   before_filter :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.order('created_at DESC')
+    @posts = Post.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
   end
 
   def show
