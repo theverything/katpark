10ec3fe4f576fc31b680ddd0178b2afae26eadf6
theverything <j3ffhorn@gmail.com>
2013-12-04 09:09:55 -0800

    config sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Katpark::Application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'katpark.c66.me',
     :enable_starttls_auto => true
   }
 
