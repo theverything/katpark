d8b1e8ec9d0adf10d9573f5445414a3977012942
Jeffrey Horn <j3ffhorn@gmail.com>
2023-05-22 15:05:06 -0700

    front page grid work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    assets: true,
+  },
+  image: {
+    service: sharpImageService(),
+  },
+});
